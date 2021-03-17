e841e46056a7338180ac6cc42e6959cbee05f59f
lukaskreibig <lukatze@posteo.de>
2021-03-17 10:24:11 +0100

    using chalk for names

---
+++ index.js
@@ -0,0 +1,5 @@
+const chalk = require('chalk');
+console.log(chalk.blue('Francesco'));
+console.log(chalk.red('Silvia'));
+console.log(chalk.green('Anna'));
+console.log(chalk.hex('#293852')('Anca'));
